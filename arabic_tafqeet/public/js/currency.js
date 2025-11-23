e78c97f1ad035e52172a194541b9ce97a263e8de
abdopcnet <abdopcnet@gmail.com>
2025-11-23 12:31:35 +0300

    🔄 currency.js

--- arabic_tafqeet/public/js/currency.js
+++ arabic_tafqeet/public/js/currency.js
@@ -3,40 +3,49 @@
 
 frappe.ui.form.on("Currency", {
 	async refresh(frm) {
-		// Only show button for SAR currency
+		// Only show buttons for SAR currency
 		if (frm.doc.name === "SAR") {
 			// Ensure SVG files are uploaded using server method
 			await ensure_svg_files_uploaded();
 			
+			// Remove existing button if any
+			$('.btn-riyal-toggle').remove();
+			
 			// Check current symbol to determine button state
-			const isBlack = frm.doc.symbol && frm.doc.symbol.includes("Saudi_Riyal_Symbol.svg");
 			const isGreen = frm.doc.symbol && frm.doc.symbol.includes("Saudi_Riyal_Symbol_green.svg");
 			
-			// Determine button label and action
-			let buttonLabel, symbolPath;
+			// Determine button color, label and action based on current state
+			let buttonClass, buttonLabel, symbolPath;
 			if (isGreen) {
-				// Currently green, next click should be black
-				buttonLabel = "Saudi Riyal Symbol Black";
+				// Currently green, button should be black to switch to black
+				buttonClass = "primary";
+				buttonLabel = __('رمز الريال باللون الأسود');
 				symbolPath = "/files/Saudi_Riyal_Symbol.svg";
 			} else {
-				// Currently black or no symbol, next click should be green
-				buttonLabel = "Saudi Riyal Symbol Green";
+				// Currently black or no symbol, button should be green to switch to green
+				buttonClass = "success";
+				buttonLabel = __('رمز الريال باللون الأخضر');
 				symbolPath = "/files/Saudi_Riyal_Symbol_green.svg";
 			}
 			
-			// Add custom button
-			frm.add_custom_button(buttonLabel, function() {
-				// Set all required fields
-				frm.set_value("fraction", "halala");
-				frm.set_value("fraction_units", 100);
-				frm.set_value("smallest_currency_fraction_value", 0.01);
-				frm.set_value("symbol", `<img src="${symbolPath}" style="height: 1.25em; width: 1.5em !important; vertical-align: middle;">`);
-				frm.set_value("symbol_on_right", 0);
-				frm.set_value("number_format", "#,###.##");
-				
-				// Save the form
-				frm.save();
-			}, __("Actions"));
+			// Add dynamic button
+			frm.page.add_inner_button(
+				buttonLabel,
+				function() {
+					// Set all required fields
+					frm.set_value("fraction", "halala");
+					frm.set_value("fraction_units", 100);
+					frm.set_value("smallest_currency_fraction_value", 0.01);
+					frm.set_value("symbol", `<img src="${symbolPath}" style="height: 1.25em; width: 1.5em !important; vertical-align: middle;">`);
+					frm.set_value("symbol_on_right", 0);
+					frm.set_value("number_format", "#,###.##");
+					
+					// Save the form
+					frm.save();
+				},
+				null,
+				buttonClass
+			).addClass("btn-riyal-toggle");
 		}
 	}
 });
